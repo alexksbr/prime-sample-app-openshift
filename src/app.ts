178fd60d4e75421179ef4fc5b94baae24a58b190
Alexander Kaserbacher <alex.kaserbacher@icloud.com>
2022-11-22 17:11:04 +0100

    change port

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send("TS App is Running");
 });
-const PORT = 4000;
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
